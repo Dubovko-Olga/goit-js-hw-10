83126f6212f2fd5aed05c5aef056586732286a81
olga-dubovko <dubovkoolga70@gmail.com>
2025-09-17 19:03:43 +0200

    add .

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,37 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
-import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
     root: 'src',
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: {
-          timer: resolve(__dirname, 'src/1-timer.html'),
-          snackbar: resolve(__dirname, 'src/2-snackbar.html'),
-         
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
       outDir: '../dist',
@@ -22,7 +39,7 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/*.html']),
+      FullReload(['./src/**/**.html']),
       SortCss({
         sort: 'mobile-first',
       }),
